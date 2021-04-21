b5321714c285e5709ed5d054cc9d4152d3cc521d
JMixson <jmixson1179@gmail.com>
2021-04-20 21:44:39 -0600

    Remove mobile theme of text editor

--- js/tinymce.js
+++ js/tinymce.js
@@ -7,8 +7,5 @@ tinymce.init({
     'searchreplace visualblocks code fullscreen',
     'insertdatetime media table paste code help wordcount',
   ],
-  mobile: { 
-    theme: 'mobile' 
-  },
   toolbar: 'insert | undo redo |  formatselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
  });
